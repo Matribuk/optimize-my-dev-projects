7824c76824341bda9f2665ee2d162df2b7ad8e40
Matribuk <antonin.leprest@epitech.eu>
2026-01-04 18:25:16 +0100

    Initial commit: iOS app with SwiftUI, Swift Testing and SwiftData

---
+++ optimize-my-dev-projects/optimize-my-dev-projectsTests/optimize_my_dev_projectsTests.swift
@@ -0,0 +1,16 @@
+//
+//  optimize_my_dev_projectsTests.swift
+//  optimize-my-dev-projectsTests
+//
+//  Created by Antonin Leprest on 04/01/2026.
+//
+
+import Testing
+
+struct optimize_my_dev_projectsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
